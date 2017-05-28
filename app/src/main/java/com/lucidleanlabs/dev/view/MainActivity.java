9a29508012dfe38d726d2aa261a3e33dfb6afba5
ramky <ramky.lll@outlook.com>
2017-05-29 04:07:55 +0530

    some changes

--- app/src/main/java/com/lucidleanlabs/dev/view/MainActivity.java
+++ app/src/main/java/com/lucidleanlabs/dev/view/MainActivity.java
@@ -2,19 +2,15 @@ package com.lucidleanlabs.dev.view;
 
 import android.content.DialogInterface;
 import android.graphics.Rect;
-import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.text.SpannableString;
-import android.text.style.StyleSpan;
 import android.text.style.UnderlineSpan;
 import android.util.Log;
 import android.view.Display;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.getkeepsafe.taptargetview.TapTarget;
@@ -29,43 +25,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.inflateMenu(R.menu.menu_main);
-        toolbar.setNavigationIcon(ContextCompat.getDrawable(this, R.drawable.ic_arrow_back_white_24dp));
-
         final Display display = getWindowManager().getDefaultDisplay();
-        final Drawable droid = ContextCompat.getDrawable(this,R.mipmap.ic_fav);
-        final Drawable droid2 = ContextCompat.getDrawable(this,R.mipmap.ic_menu_share);
-        final Drawable droid3 = ContextCompat.getDrawable(this,R.mipmap.ic_3d);
-        final Drawable droid4  = ContextCompat.getDrawable(this,R.mipmap.ic_download);
+        final Drawable droid = ContextCompat.getDrawable(this, R.mipmap.ic_fav);
+        final Drawable droid2 = ContextCompat.getDrawable(this, R.mipmap.ic_menu_share);
+        final Drawable droid3 = ContextCompat.getDrawable(this, R.mipmap.ic_3d);
+        final Drawable droid4 = ContextCompat.getDrawable(this, R.mipmap.ic_download);
 
-        final Rect droidTarget = new Rect(0,0,droid.getIntrinsicWidth()*2,droid.getIntrinsicHeight()*2);
+        final Rect droidTarget = new Rect(0, 0, droid.getIntrinsicWidth() * 2, droid.getIntrinsicHeight() * 2);
+        final Rect droidTarget2 = new Rect(2, 2, droid2.getIntrinsicWidth() * 2, droid2.getIntrinsicHeight() * 2);
+        final Rect droidTarget3 = new Rect(2, 4, droid3.getIntrinsicWidth() * 2, droid3.getIntrinsicHeight() * 2);
+        final Rect droidTarget4 = new Rect(0, 0, droid4.getIntrinsicWidth() * 2, droid4.getIntrinsicHeight() * 2);
+
         droidTarget.offset(display.getWidth() / 2, display.getHeight() / 2);
-
-
-        final SpannableString sassyDesc = new SpannableString("It allows you to go back, sometimes");
-        sassyDesc.setSpan(new StyleSpan(Typeface.ITALIC), sassyDesc.length() - "somtimes".length(), sassyDesc.length(), 0);
+        droidTarget2.offset(display.getWidth() / 2, display.getHeight() / 2);
+        droidTarget3.offset(display.getWidth() / 2, display.getHeight() / 2);
+        droidTarget4.offset(display.getWidth() / 2, display.getHeight() / 2);
 
 
         final TapTargetSequence sequence = new TapTargetSequence(this)
                 .targets(
                         // This tap target will target the back button, we just need to pass its containing toolbar
-                        TapTarget.forToolbarNavigationIcon(toolbar, "This is the back button", sassyDesc).id(1),
-                        // Likewise, this tap target will target the search button
-                        TapTarget.forToolbarMenuItem(toolbar, R.id.search, "This is a search icon", "As you can see, it has gotten pretty dark around here...")
-                                .dimColor(android.R.color.black)
-                                .outerCircleColor(R.color.colorAccent)
-                                .targetCircleColor(android.R.color.black)
-                                .transparentTarget(true)
-                                .textColor(android.R.color.black)
-                                .id(2),
-                        // You can also target the overflow button in your toolbar
-                        TapTarget.forToolbarOverflow(toolbar, "This will show more options", "But they're not useful :(").id(3),
-                        // This tap target will target our droid buddy at the given target rect
-                        TapTarget.forBounds(droidTarget, "Oh look!", "You can point to any part of the screen. You also can't cancel this one!")
+                        TapTarget.forBounds(droidTarget2, "welcome", "hello how are u man")
                                 .cancelable(false)
-                                .icon(droid).icon(droid2).icon(droid3).icon(droid4)
-                                .id(4)
+                                .icon(droid2)
+                                .id(1),
+                        TapTarget.forBounds(droidTarget3, "nice to see u", "yup it is true")
+                                .cancelable(false)
+                                .icon(droid3)
+                                .id(2),
+                        TapTarget.forBounds(droidTarget4, "qwertyuiop", "asdfghjkl")
+                                .cancelable(false)
+                                .icon(droid4)
+                                .id(3)
                 )
                 .listener(new TapTargetSequence.Listener() {
                     // This listener will tell us when interesting(tm) events happen in regards
@@ -98,10 +89,8 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-        // You don't always need a sequence, and for that there's a single time tap target
-        final SpannableString spannedDesc = new SpannableString("This is the sample app for TapTargetView");
-        spannedDesc.setSpan(new UnderlineSpan(), spannedDesc.length() - "TapTargetView".length(), spannedDesc.length(), 0);
-        TapTargetView.showFor(this, TapTarget.forView(findViewById(R.id.article_fav_icon), "Hello, world!", spannedDesc)
+
+        TapTargetView.showFor(this, TapTarget.forView(findViewById(R.id.article_fav_icon), "Hello, world!")
                 .cancelable(false)
                 .drawShadow(true)
                 .tintTarget(false), new TapTargetView.Listener() {
@@ -123,5 +112,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("TapTargetViewSample", "You dismissed me :(");
             }
         });
+
+
     }
 }
